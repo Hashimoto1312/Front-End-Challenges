2de8d5bec1d37f6fac3840e01dd2df9b873596e3
橋本ケンジ <kendi.campos@hotmail.com>
2022-11-08 19:35:59 -0300

    Tailwind setup

--- FAQ accordion card/src/tailwind.config.cjs
+++ FAQ accordion card/src/tailwind.config.cjs
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["../index.html"],
   theme: {
-    extend: {},
+    colors: {
+      gradient:
+        "linear-gradient(180deg, rgba(175,103,233,1) 0%, rgba(101,101,231,1) 70%)",
+      "very dark desaturated blue": "#1d1e35",
+      "soft red": "#f47c57",
+      "very dark grayish blue": "#4a4b5e",
+      "dark grayish blue": "#787887",
+      "light grayish blue": "#e7e7e9",
+      "color": "#ffffff",
+      "background color": "#242424",
+    },
   },
   plugins: [],
 }
